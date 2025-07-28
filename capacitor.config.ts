980cf18b6fda129d01dd6811b0e04c39b1c6bca4
MasterSe7en168 <MasterSe7en168@gmail.com>
2025-07-28 16:08:19 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tutormatch.app',
+  appName: 'TutorMatch',
+  webDir: 'dist/public'
+};
+
+export default config;
